7526c8756290c30a138686cbbf07eeeec1611bf9
pyounani <yndbsk9372@gmail.com>
2024-04-20 11:30:43 +0900

    Fix: UserEntity 기본 생성자 추가

--- src/main/java/com/cojac/storyteller/domain/UserEntity.java
+++ src/main/java/com/cojac/storyteller/domain/UserEntity.java
@@ -2,9 +2,11 @@ package com.cojac.storyteller.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class UserEntity {
 
     @Id
