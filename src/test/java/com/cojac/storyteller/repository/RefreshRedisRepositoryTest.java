7b068f8a1744447a26f6d7b542fb50fb34feb413
pyounani <yndbsk9372@gmail.com>
2024-04-22 11:46:45 +0900

    Test: Refresh 리포지토리 코드 중복 @AfterEach로 분리

--- src/test/java/com/cojac/storyteller/repository/RefreshRedisRepositoryTest.java
+++ src/test/java/com/cojac/storyteller/repository/RefreshRedisRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.cojac.storyteller.repository;
 
 import com.cojac.storyteller.domain.RefreshEntity;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,7 @@ class RefreshRedisRepositoryTest {
 
     @Autowired
     private RefreshRedisRepository refreshRedisRepository;
+    private RefreshEntity savedRefresh; // 각 테스트에서 생성된 엔티티를 추적하는 필드
 
     @Test
     void testSaveAndFind() {
@@ -21,7 +23,7 @@ class RefreshRedisRepositoryTest {
         RefreshEntity refreshEntity = new RefreshEntity("refresh", "username");
 
         // when
-        RefreshEntity savedRefresh = refreshRedisRepository.save(refreshEntity);
+        savedRefresh = refreshRedisRepository.save(refreshEntity);
         String refresh = savedRefresh.getRefresh();
         Optional<RefreshEntity> foundRefresh = refreshRedisRepository.findById(refresh);
 
@@ -32,16 +34,13 @@ class RefreshRedisRepositoryTest {
         assertNotNull(foundRefresh);
         assertEquals("username", foundRefresh.get().getUsername());
         assertEquals("refresh", foundRefresh.get().getRefresh());
-
-        // 관련한 테스트용 데이터 삭제
-        refreshRedisRepository.delete(savedRefresh);
     }
 
     @Test
     void testDelete() {
         // given
         RefreshEntity refreshEntity = new RefreshEntity("refresh", "username");
-        RefreshEntity savedRefresh = refreshRedisRepository.save(refreshEntity);
+        savedRefresh = refreshRedisRepository.save(refreshEntity);
         String refresh = savedRefresh.getRefresh();
         System.out.println(refresh);
 
@@ -49,7 +48,15 @@ class RefreshRedisRepositoryTest {
         refreshRedisRepository.deleteById(refresh);
 
         // then
-        assertNull(refreshRedisRepository.findById(refresh));
+        assertFalse(refreshRedisRepository.findById(refresh).isPresent());
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (savedRefresh != null) {
+            refreshRedisRepository.delete(savedRefresh);
+        }
+        savedRefresh = null; // 필드를 null로 초기화하여 재사용 가능하게 만듭니다.
     }
 
 }
