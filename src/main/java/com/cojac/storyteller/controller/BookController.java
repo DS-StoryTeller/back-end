fddf6469cb47f507bb2976ed1da1bb95d4df2678
devdaradara <devdaradara@gmail.com>
2024-04-23 23:36:09 +0900

    Refactor: Response 응답 방식 통일

--- src/main/java/com/cojac/storyteller/controller/BookController.java
+++ src/main/java/com/cojac/storyteller/controller/BookController.java
@@ -1,9 +1,12 @@
 package com.cojac.storyteller.controller;
 
+import com.cojac.storyteller.code.ErrorCode;
 import com.cojac.storyteller.code.ResponseCode;
 import com.cojac.storyteller.dto.book.BookDTO;
 import com.cojac.storyteller.dto.book.CreateBookRequest;
+import com.cojac.storyteller.dto.response.ErrorResponseDTO;
 import com.cojac.storyteller.dto.response.ResponseDTO;
+import com.cojac.storyteller.exception.ProfileNotFoundException;
 import com.cojac.storyteller.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +20,10 @@ public class BookController {
     private final BookService bookService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO<BookDTO>> createBook(@RequestBody CreateBookRequest request, @RequestParam Integer profileId) {
+    public ResponseEntity<ResponseDTO> createBook(@RequestBody CreateBookRequest request, @RequestParam Integer profileId) {
         BookDTO createdBook = bookService.createBook(request.getTitle(), request.getContent(), profileId);
-        ResponseDTO<BookDTO> response = new ResponseDTO<>(ResponseCode.SUCCESS_CREATE_BOOK, createdBook);
-        return ResponseEntity.status(ResponseCode.SUCCESS_CREATE_BOOK.getStatus()).body(response);
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_CREATE_BOOK.getStatus().value())
+                .body(new ResponseDTO<>(ResponseCode.SUCCESS_CREATE_BOOK, createdBook));
     }
 }
