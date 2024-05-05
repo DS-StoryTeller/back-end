a119c605c6689d64fe4f1a49ea9134036f9d1c1b
devdaradara <devdaradara@gmail.com>
2024-05-06 00:51:13 +0900

    Refactor: 에러 코드 파트 메인 머지 수정

--- src/main/java/com/cojac/storyteller/code/ErrorCode.java
+++ src/main/java/com/cojac/storyteller/code/ErrorCode.java
@@ -14,6 +14,13 @@ public enum ErrorCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
     /**
+     * 401 UNAUTHORIZED: 토큰 만료
+     */
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다."),
+    TOKEN_MISSING(HttpStatus.UNAUTHORIZED, "요청 헤더에 토큰이 없습니다."),
+
+    /**
      * 404 NOT_FOUND: 리소스를 찾을 수 없음
      */
     PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "프로필을 찾을 수 없습니다."),
