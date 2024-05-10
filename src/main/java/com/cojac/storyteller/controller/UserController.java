071bb65f48f233404221e42c957e3353384efc63
pyounani <yndbsk9372@gmail.com>
2024-05-10 17:11:42 +0900

    Feat: 소셜 로그인 작동 확인을 위한 컨트롤러와 HTML 페이지 #14

--- src/main/java/com/cojac/storyteller/controller/UserController.java
+++ src/main/java/com/cojac/storyteller/controller/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
