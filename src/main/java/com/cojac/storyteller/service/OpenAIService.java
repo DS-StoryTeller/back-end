9442bd968ddefa5f19cf3baca7211fc755d44143
7beunseo <kimes0403@gmail.com>
2024-06-25 14:10:29 +0900

    Comment: 주석 수정

--- src/main/java/com/cojac/storyteller/service/OpenAIService.java
+++ src/main/java/com/cojac/storyteller/service/OpenAIService.java
@@ -49,7 +49,7 @@ public class OpenAIService {
         String url = "https://api.openai.com/v1/chat/completions";
         CompletionRequestDto.Message message = CompletionRequestDto.Message.builder()
                 .role("user")
-                // 제목과 내용을 Title: 과 Content: 로 구분하여 요청
+                // 퀴즈 3개를 \n으로 구분하여 요청
                 .content(story + "라는 동화 내용이 있어. 이 내용에 대해 창의력을 향상시킬 수 있는 질문 3가지를 한국어 존댓말로 알려줘. " +
                         "퀴즈 3개는 \n으로 구분해줘. " +
                         "질문의 난이도는 " + age + "세 연령대에 맞게 생성해줘. "
