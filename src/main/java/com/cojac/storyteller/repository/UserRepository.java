0e3ec4dabf4b2f66bd58319e67fe4781243a50d3
pyounani <yndbsk9372@gmail.com>
2024-06-28 12:34:14 +0900

    Rename: 자체 로그인과 소셜 로그인 구분을 위한 파일명 수정

--- src/main/java/com/cojac/storyteller/repository/UserRepository.java
+++ src/main/java/com/cojac/storyteller/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.cojac.storyteller.repository;
 
-import com.cojac.storyteller.domain.UserEntity;
+import com.cojac.storyteller.domain.LocalUserEntityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<LocalUserEntityEntity, Integer> {
 
     Boolean existsByUsername(String username);
 
-    Optional<UserEntity> findByUsername(String username);
+    Optional<LocalUserEntityEntity> findByUsername(String username);
 }
