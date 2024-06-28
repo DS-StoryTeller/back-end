ed5f7483d30ad665f21539bca003eda42809c825
pyounani <yndbsk9372@gmail.com>
2024-06-28 12:35:25 +0900

    Feat: 프로필 생성을 위한 컨트롤러 구현

--- src/main/java/com/cojac/storyteller/controller/ProfileController.java
+++ src/main/java/com/cojac/storyteller/controller/ProfileController.java
@@ -1,20 +1,18 @@
 package com.cojac.storyteller.controller;
 
 import com.cojac.storyteller.code.ResponseCode;
+import com.cojac.storyteller.dto.profile.ProfileDTO;
 import com.cojac.storyteller.dto.profile.ProfilePhotoDTO;
 import com.cojac.storyteller.dto.response.ResponseDTO;
 import com.cojac.storyteller.service.ProfileService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/profile")
+@RequestMapping("/profiles")
 @RequiredArgsConstructor
 public class ProfileController {
 
@@ -31,14 +29,17 @@ public class ProfileController {
                 .body(new ResponseDTO<>(ResponseCode.SUCCESS_PROFILE_PHOTOS, result));
     }
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity createProfile(
-            @RequestPart(value = "name") String name,
-            @RequestPart(value = "file", required = false) MultipartFile file
-    ) {
-        profileService.createProfile(name, file);
-        return new ResponseEntity(null, HttpStatus.OK);
+    /**
+     * 프로필 생성하기
+     */
+    @PostMapping
+    public ResponseEntity<ResponseDTO> createProfile(@RequestBody ProfileDTO profileDTO) {
+        ProfileDTO result = profileService.createProfile(profileDTO);
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_CREATE_PROFILE.getStatus().value())
+                .body(new ResponseDTO<>(ResponseCode.SUCCESS_CREATE_PROFILE, result));
     }
+
 
 
 }
