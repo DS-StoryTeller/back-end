9cb287b71ed9016f404f83a40e3ff7293a5e18a3
pyounani <yndbsk9372@gmail.com>
2024-09-05 13:53:24 +0900

    Feat: 책 설정 문서화 작업 완료

--- src/main/java/com/cojac/storyteller/controller/SettingController.java
+++ src/main/java/com/cojac/storyteller/controller/SettingController.java
@@ -4,6 +4,11 @@ import com.cojac.storyteller.code.ResponseCode;
 import com.cojac.storyteller.dto.response.ResponseDTO;
 import com.cojac.storyteller.dto.setting.SettingDTO;
 import com.cojac.storyteller.service.SettingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +16,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/settings")
 @RequiredArgsConstructor
+@Tag(name = "Setting Controller", description = "설정 관련 API")
 public class SettingController {
     private final SettingService settingService;
 
+    /**
+     * 책 설정 업데이트
+     */
     @PutMapping("/update")
+    @Operation(
+            summary = "책 설정 업데이트",
+            description = "책 설정 업데이트 API",
+            parameters = {
+                    @Parameter(name = "profileId", in = ParameterIn.QUERY, description = "프로필 ID", required = true),
+                    @Parameter(name = "bookId", in = ParameterIn.QUERY, description = "책 ID", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "책 설정을 성공적으로 변경했습니다", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json"))
+            }
+    )
     public ResponseEntity<ResponseDTO<SettingDTO>> updateSettings(
             @RequestParam Integer profileId,
             @RequestParam Integer bookId,
@@ -23,7 +43,21 @@ public class SettingController {
         return ResponseEntity.ok(new ResponseDTO<>(ResponseCode.SUCCESS_UPDATE_SETTING, response));
     }
 
+    /**
+     * 책 설정 조회
+     */
     @GetMapping("/detail")
+    @Operation(
+            summary = "책 설정 조회",
+            description = "책 설정 조회 API",
+            parameters = {
+                    @Parameter(name = "profileId", in = ParameterIn.QUERY, description = "프로필 ID", required = true),
+                    @Parameter(name = "bookId", in = ParameterIn.QUERY, description = "책 ID", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "책 설정을 성공적으로 조회했습니다", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json"))
+            }
+    )
     ResponseEntity<ResponseDTO<SettingDTO>> getDetailSettings(
             @RequestParam Integer profileId,
             @RequestParam Integer bookId) {
