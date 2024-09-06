3c69dd80cea0bb02aa0ca4b6e299895d43e9cc7c
pyounani <yndbsk9372@gmail.com>
2024-09-06 10:58:07 +0900

    Feat: Swagger 자동 문서화 작업 완료 - USER

--- src/main/java/com/cojac/storyteller/controller/UserController.java
+++ src/main/java/com/cojac/storyteller/controller/UserController.java
@@ -4,10 +4,18 @@ import com.cojac.storyteller.code.ResponseCode;
 import com.cojac.storyteller.dto.response.ResponseDTO;
 import com.cojac.storyteller.dto.user.*;
 import com.cojac.storyteller.service.UserService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -23,6 +31,7 @@ import java.util.Iterator;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "User Controller", description = "유저 관련 API")
 public class UserController {
 
     private final UserService userService;
@@ -31,8 +40,15 @@ public class UserController {
      * 자체 회원가입
      */
     @PostMapping("/register")
-    public ResponseEntity<ResponseDTO> registerUser(@Valid LocalUserDTO localUserDTO) {
-        LocalUserDTO res = userService.registerUser(localUserDTO);
+    @Operation(
+            summary = "자체 회원가입",
+            description = "자체 회원가입 API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "회원가입을 성공했습니다."),
+            }
+    )
+    public ResponseEntity<ResponseDTO> registerUser(@ParameterObject @Valid CreateUserRequestDTO createUserRequestDTO) {
+        LocalUserDTO res = userService.registerUser(createUserRequestDTO);
         return ResponseEntity
                 .status(ResponseCode.SUCCESS_REGISTER.getStatus().value())
                 .body(new ResponseDTO<>(ResponseCode.SUCCESS_REGISTER, res));
@@ -42,6 +58,24 @@ public class UserController {
      * 아이디 중복 확인
      */
     @PostMapping("/username/verifications")
+    @Operation(
+            summary = "아이디 중복 확인",
+            description = "아이디 중복 확인 API",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "확인할 아이디 정보",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(
+                                            value = "{\"username\": \"사용자의 username\"}"
+                                    )
+                            }
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "아이디가 사용 검증 완료했습니다. authResult를 확인해주세요."),
+            }
+    )
     public ResponseEntity<ResponseDTO> verifiedUsername(@Valid @RequestBody UsernameDTO usernameDTO) {
 
         UsernameDTO res = userService.verifiedUsername(usernameDTO);
@@ -54,6 +88,29 @@ public class UserController {
      * 토큰 재발급
      */
     @PostMapping("/reissue")
+    @Operation(
+            summary = "토큰 재발급",
+            description = "토큰 재발급 API",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "재발급 받은 아이디 정보",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(
+                                            name = "자체 로그인 사용자일 경우",
+                                            value = "{\"username\": \"사용자의 username\"}"
+                                    ),
+                                    @ExampleObject(
+                                            name = "소셜 로그인 사용자일 경우",
+                                            value = "{\"accountId\": \"사용자의 accountId\"}"
+                                    )
+                            }
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "토큰 재발급을 성공했습니다."),
+            }
+    )
     public ResponseEntity<ResponseDTO> reissueAccessToken(HttpServletRequest request,
                                                HttpServletResponse response,
                                                @Valid @RequestBody ReissueDTO reissueDTO) throws IOException {
@@ -67,6 +124,24 @@ public class UserController {
      * 이메일 인증 코드 요청하기
      */
     @PostMapping("/emails/verification-requests")
+    @Operation(
+            summary = "이메일 인증 코드 요청하기",
+            description = "이메일 인증 코드 요청 API",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "인증 코드 요청받을 이메일",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(
+                                            value = "{\"email\": \"사용자의 email\"}"
+                                    )
+                            }
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "해당 이메일로 인증 코드가 전송되었습니다."),
+            }
+    )
     public ResponseEntity<ResponseDTO> sendEmailVerification(@Valid @RequestBody EmailDTO emailDTO) {
 
         userService.sendCodeToEmail(emailDTO.getEmail());
@@ -79,6 +154,24 @@ public class UserController {
      * 인증 코드 확인하기
      */
     @PostMapping("/emails/verifications")
+    @Operation(
+            summary = "인증 코드 확인하기",
+            description = "인증 코드 확인 API",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "검즈을 위한 이메일과 인증 코드",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(
+                                            value = "{\"email\": \"사용자의 email\", \"authCode\": \"인증코드\"}"
+                                    )
+                            }
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "인증 코드가 검증 완료했습니다. authResult를 확인해주세요."),
+            }
+    )
     public ResponseEntity<ResponseDTO> verificationEmailCode(@Valid @RequestBody EmailDTO emailDTO) {
 
         EmailDTO res = userService.verifiedCode(emailDTO.getEmail(), emailDTO.getAuthCode());
@@ -88,6 +181,7 @@ public class UserController {
     }
 
     // 로그인 이후 유저 아이디 및 role 확인 방법
+    @Hidden
     @GetMapping("test")
     public ResponseEntity test() {
         // 사용자 아이디(username)
