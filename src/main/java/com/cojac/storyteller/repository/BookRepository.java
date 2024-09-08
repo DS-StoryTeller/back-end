f23d22a054870c14c0e464f9a4d99e111699413f
pyounani <yndbsk9372@gmail.com>
2024-09-08 09:08:29 +0900

    Fix: 양방향 없애기 위해 페치 조인으로 해결한 부분 삭제

--- src/main/java/com/cojac/storyteller/repository/BookRepository.java
+++ src/main/java/com/cojac/storyteller/repository/BookRepository.java
@@ -2,16 +2,14 @@ package com.cojac.storyteller.repository;
 
 import com.cojac.storyteller.domain.BookEntity;
 import com.cojac.storyteller.domain.ProfileEntity;
-import com.cojac.storyteller.repository.queryDSL.BookRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface BookRepository extends JpaRepository<BookEntity, Integer>, BookRepositoryCustom {
+public interface BookRepository extends JpaRepository<BookEntity, Integer> {
     List<BookEntity> findByProfile(ProfileEntity profile);
 
     Optional<BookEntity> findByIdAndProfile(Integer id, ProfileEntity profile);
--- src/main/java/com/cojac/storyteller/service/BookService.java
+++ src/main/java/com/cojac/storyteller/service/BookService.java
@@ -83,7 +83,7 @@ public class BookService {
         ProfileEntity profile = profileRepository.findById(profileId)
                 .orElseThrow(() -> new ProfileNotFoundException(ErrorCode.PROFILE_NOT_FOUND));
 
-        List<BookEntity> books = bookRepository.findFavoriteBooksByProfile(profile);
+        List<BookEntity> books = bookRepository.findByProfileAndIsFavoriteTrue(profile);
         return BookMapper.mapToBookListResponseDTOs(books);
     }
 
