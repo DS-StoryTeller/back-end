34a3900b18bcbbc1ee8ad645ab1b7700e09d4c9c
pyounani <yndbsk9372@gmail.com>
2024-09-08 09:06:29 +0900

    Remove: 사용하지 않는 import문 삭제

--- src/main/java/com/cojac/storyteller/repository/queryDSL/BookRepositoryCustom.java
+++ src/main/java/com/cojac/storyteller/repository/queryDSL/BookRepositoryCustom.java
@@ -2,7 +2,6 @@ package com.cojac.storyteller.repository.queryDSL;
 
 import com.cojac.storyteller.domain.BookEntity;
 import com.cojac.storyteller.domain.ProfileEntity;
-import com.cojac.storyteller.dto.book.BookListResponseDTO;
 
 import java.util.List;
 
