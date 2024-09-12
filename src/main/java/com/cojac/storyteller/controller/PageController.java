6cf27a323ddc555b4b5707ed0077030d5c5330db
pyounani <yndbsk9372@gmail.com>
2024-09-12 11:03:03 +0900

    Fix: 페이지 컨트롤러에서 Swagger 독스용 인터페이스 추가

--- src/main/java/com/cojac/storyteller/controller/PageController.java
+++ src/main/java/com/cojac/storyteller/controller/PageController.java
@@ -1,23 +1,27 @@
 package com.cojac.storyteller.controller;
 
 import com.cojac.storyteller.code.ResponseCode;
+import com.cojac.storyteller.docs.PageControllerDocs;
 import com.cojac.storyteller.dto.page.PageDetailResponseDTO;
 import com.cojac.storyteller.dto.request.PageRequestDTO;
 import com.cojac.storyteller.dto.response.ResponseDTO;
 import com.cojac.storyteller.service.PageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pages")
 @RequiredArgsConstructor
-public class PageController {
+public class PageController implements PageControllerDocs {
     private final PageService pageService;
 
     @GetMapping("/detail")
-    public ResponseDTO<PageDetailResponseDTO> getPageDetail(@ModelAttribute PageRequestDTO pageRequestDTO) {
+    public ResponseEntity<ResponseDTO> getPageDetail(@ModelAttribute PageRequestDTO pageRequestDTO) {
         PageDetailResponseDTO pageDetail = pageService.getPageDetail(pageRequestDTO);
-        return new ResponseDTO<>(ResponseCode.SUCCESS_RETRIEVE_PAGE_DETAILS, pageDetail);
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_RETRIEVE_PAGE_DETAILS.getStatus().value())
+                .body(new ResponseDTO<>(ResponseCode.SUCCESS_RETRIEVE_PAGE_DETAILS, pageDetail));
     }
 
 }
