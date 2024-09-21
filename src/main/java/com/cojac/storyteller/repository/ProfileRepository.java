9c43f231a42be921e586b7dde80318c57d31c6e1
pyounani <yndbsk9372@gmail.com>
2024-09-21 10:57:01 +0900

    Remove: 사용하지 않는 메소드 삭제

--- src/main/java/com/cojac/storyteller/repository/ProfileRepository.java
+++ src/main/java/com/cojac/storyteller/repository/ProfileRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProfileRepository extends JpaRepository<ProfileEntity, Integer> {
-    
+
     List<ProfileEntity> findByUser(UserEntity user);
 }
