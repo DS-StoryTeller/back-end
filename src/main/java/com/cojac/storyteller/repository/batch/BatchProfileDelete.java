019137b559868cf226f0806834f920f297a6c1e0
pyounani <yndbsk9372@gmail.com>
2024-09-30 11:59:56 +0900

    HotFix: DB 테이블명 수정

--- src/main/java/com/cojac/storyteller/repository/batch/BatchProfileDelete.java
+++ src/main/java/com/cojac/storyteller/repository/batch/BatchProfileDelete.java
@@ -26,35 +26,21 @@ public class BatchProfileDelete {
             // 각 책에 대한 설정 ID를 조회
             List<Integer> settingIds = jdbcTemplate.queryForList("SELECT setting_id FROM BookEntity WHERE id IN (?)", Integer.class, bookIds);
 
-            // 각 책의 페이지 ID 목록 조회
-            List<Integer> pageIds = jdbcTemplate.queryForList("SELECT id FROM PageEntity WHERE book_id IN (?)", Integer.class, bookIds);
+            // 모르는 단어 삭제
+            jdbcTemplate.batchUpdate(
+                    "DELETE FROM UnknownWordEntity WHERE page_id IN (SELECT id FROM PageEntity WHERE book_id = ?)",
+                    new BatchPreparedStatementSetter() {
+                        @Override
+                        public void setValues(PreparedStatement ps, int i) throws SQLException {
+                            ps.setInt(1, bookIds.get(i));
+                        }
 
-            // 각 페이지에 대한 모르는 단어가 있는지 확인 후 삭제
-            if (!pageIds.isEmpty()) {
-                List<Integer> unknownWordPageIds = jdbcTemplate.queryForList(
-                        "SELECT DISTINCT page_id FROM UnknownWordEntity WHERE page_id IN (?)",
-                        Integer.class,
-                        pageIds
-                );
-
-                // 모르는 단어 삭제 (존재하는 경우에만)
-                if (!unknownWordPageIds.isEmpty()) {
-                    jdbcTemplate.batchUpdate(
-                            "DELETE FROM UnknownWordEntity WHERE page_id IN (?)",
-                            new BatchPreparedStatementSetter() {
-                                @Override
-                                public void setValues(PreparedStatement ps, int i) throws SQLException {
-                                    ps.setInt(1, unknownWordPageIds.get(i));
-                                }
-
-                                @Override
-                                public int getBatchSize() {
-                                    return unknownWordPageIds.size();
-                                }
-                            }
-                    );
-                }
-            }
+                        @Override
+                        public int getBatchSize() {
+                            return bookIds.size();
+                        }
+                    }
+            );
 
             // 페이지 삭제
             jdbcTemplate.batchUpdate(
@@ -108,4 +94,5 @@ public class BatchProfileDelete {
         // 프로필 삭제
         jdbcTemplate.update("DELETE FROM ProfileEntity WHERE id = ?", profileId);
     }
+
 }
