f6409adc32e020c386605d6102c945b3033cbd1d
pyounani <yndbsk9372@gmail.com>
2024-10-09 16:01:32 +0900

    동화 서비스계층 테스트 케이스 추가

--- src/test/java/com/cojac/storyteller/service/ProfileServiceTest.java
+++ src/test/java/com/cojac/storyteller/service/ProfileServiceTest.java
@@ -17,9 +17,11 @@ import com.cojac.storyteller.repository.batch.BatchProfileDelete;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.multipart.MultipartFile;
 
